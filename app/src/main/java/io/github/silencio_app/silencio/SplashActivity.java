ad692970f9dd385c7f82ddea05c464e0a5301b31
Divay Prakash <divayprakash3@gmail.com>
2016-10-22 05:35:06 +0530

    Add custom font to splash screen

--- app/src/main/java/io/github/silencio_app/silencio/SplashActivity.java
+++ app/src/main/java/io/github/silencio_app/silencio/SplashActivity.java
@@ -1,9 +1,11 @@
 package io.github.silencio_app.silencio;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -13,6 +15,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TextView tx = (TextView)findViewById(R.id.textViewSilencio);
+        Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/Somatic-Rounded.otf");
+        tx.setTypeface(custom_font);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
