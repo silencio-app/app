2edbb21aa23bc15ea65d19fc7db108d7fbebffe9
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-10-22 11:24:23 +0530

    some design tweaks , added a sound meter with three different colors and rotation blocked

--- app/src/main/java/io/github/silencio_app/silencio/MainActivity.java
+++ app/src/main/java/io/github/silencio_app/silencio/MainActivity.java
@@ -2,15 +2,20 @@ package io.github.silencio_app.silencio;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
+import android.content.pm.ActivityInfo;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
+import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -27,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
     private int lastX = 0;  // Pointer for plotting the amplitude
     private double amp_ref = 3.27;
     private boolean isStarted = false;
+    private static String PREVIOUS_X = "Previous x axis point";
+    private static String PREVIOUS_dB = "Previous noted decibals ";
+    private int db_level;
+    private ProgressBar db_meter;
     /**
      * Function called when start button is pressed
      * @param view
@@ -36,7 +45,6 @@ public class MainActivity extends AppCompatActivity {
         /**
          * start the MIC if mediaRecorder instance is created else Pops up a message
          */
-
         if (isStarted == false) {
             // previously invisible view
             View myView = findViewById(R.id.graph);
@@ -54,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
             // make the view visible and start the animation
             myView.setVisibility(View.VISIBLE);
+            db_meter.setVisibility(View.VISIBLE);
             anim.start();
 
             isStarted = true;
@@ -87,8 +96,9 @@ public class MainActivity extends AppCompatActivity {
     public void stopMIC(View view) {
 
         if (isStarted == true) {
+            db_meter.setVisibility(View.GONE);
             TextView tview = (TextView) findViewById(R.id.amp);
-            tview.setText("Press START to begin!");
+            tview.setText("Press START");
 
             // previously visible view
             final View myView = findViewById(R.id.graph);
@@ -101,8 +111,7 @@ public class MainActivity extends AppCompatActivity {
             float initialRadius = (float) Math.hypot(cx, cy);
 
             // create the animation (the final radius is zero)
-            Animator anim =
-                    ViewAnimationUtils.createCircularReveal(myView, cx, cy, initialRadius, 0);
+            Animator anim = ViewAnimationUtils.createCircularReveal(myView, cx, cy, initialRadius, 0);
 
             // make the view invisible when the animation is done
             anim.addListener(new AnimatorListenerAdapter() {
@@ -129,13 +138,20 @@ public class MainActivity extends AppCompatActivity {
             Log.d(MSG, "================== NO MIC LOCKED ================");
         }
     }
-
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putInt(PREVIOUS_X, lastX);
+        savedInstanceState.putInt(PREVIOUS_dB, db_level);
+        super.onSaveInstanceState(savedInstanceState);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_main);
 
         amplitude = (TextView)findViewById(R.id.amp);
+        db_meter = (ProgressBar)findViewById(R.id.db_meter);
         /**
          *  Initialising the empty graph
          */
@@ -173,12 +189,31 @@ public class MainActivity extends AppCompatActivity {
 
             // if recording flag is true then keep mapping graph
             while(recording_flag){
-                final int amp_val = getAmplitude();
+                int raw_amp_val = getAmplitude();
+                final int amp_val;
+                if (raw_amp_val < 0){
+                    amp_val = 0;
+                }
+                else{
+                    amp_val = raw_amp_val;
+                }
+                db_level = amp_val;
                 final String amp_val_string = amp_val + " dB";
                 MainActivity.this.runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         amplitude.setText(amp_val_string);
+                        db_meter.setProgress(amp_val);
+                        if (amp_val <= 70){
+                            db_meter.setProgressDrawable(getDrawable(R.drawable.greenprogress));
+                        }
+                        if (amp_val > 70 && amp_val <= 90){
+                            db_meter.setProgressDrawable(getDrawable(R.drawable.orangeprogress));
+                        }
+                        if (amp_val > 90){
+                            db_meter.setProgressDrawable(getDrawable(R.drawable.redprogress));
+                        }
+//                        db_meter.getSolidColor(getResources().getColor(R.color.colorAccent));
                         series.appendData(new DataPoint(lastX++, amp_val), true, 100);
                     }
                 });
@@ -190,6 +225,7 @@ public class MainActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
             }
+            Log.d(MSG, "======== Thread Destroyed =========");
         }
     }
 }
