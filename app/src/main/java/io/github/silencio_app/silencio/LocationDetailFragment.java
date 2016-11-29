9ca8c110c190cb816495dd2e0cd5a81f5339fb9f
Vipin Chaudhary <vipin14119@iiitd.ac.in>
2016-11-29 10:35:17 +0530

    graph is on the way

--- app/src/main/java/io/github/silencio_app/silencio/LocationDetailFragment.java
+++ app/src/main/java/io/github/silencio_app/silencio/LocationDetailFragment.java
@@ -9,6 +9,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 /**
  * Created by vipin on 29/11/16.
  */
@@ -38,6 +43,33 @@ public class LocationDetailFragment extends Fragment {
         vp_location_name.setText(passedData.getString("location_name"));
         vp_location_db.setText(passedData.getString("location_db"));
         Log.d("ASMKBDSABDA", String.valueOf(passedData.getFloatArray("location_records")));
+
+        GraphView graph_two = (GraphView) rootView.findViewById(R.id.graph_two);
+        Log.d("GRAPH", String.valueOf(graph_two));
+        float [] records = passedData.getFloatArray("location_records");
+        DataPoint[] dataPoints = new DataPoint[records.length+1];
+        if(records.length < 1){
+            dataPoints[0] = new DataPoint(0,0);
+        }
+        else{
+            for(int i=0;i<records.length;i++){
+                dataPoints[i] = new DataPoint(i, records[i]);
+            }
+        }
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3)
+        });
+        /*Log.d("GRAPH", String.valueOf(series[0]));*/
+        graph_two.addSeries(series);
+        Viewport viewport = graph_two.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setMinX(1.0);
+        viewport.setMinY(0);  // min value is 0
+        viewport.setMaxY(100);  // max value is 32768
+        viewport.setMaxX(100);  // 10 units frame
+        viewport.setScalable(true); // auto scroll to right
 
         return rootView;
     }
